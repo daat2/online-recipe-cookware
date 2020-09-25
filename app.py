fc460c87e63e522ea67a510c350881eca7bd7200
daat2 <ibi.olu@gmail.com>
2020-09-25 17:38:15 +0000

    Update to Home Nav Link

--- app.py
+++ app.py
@@ -17,10 +17,10 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/get_recipes")
-def get_recipes():
+@app.route("/home")
+def home():
     recipes = mongo.db.recipes.find()
-    return render_template("recipes.html", recipes=recipes)
+    return render_template("index.html", recipes=recipes)
 
 
 @app.route("/register", methods=["GET", "POST"])
